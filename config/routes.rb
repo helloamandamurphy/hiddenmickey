d3b7a75b399baca99ba172752bde3ea846c179e6
Amanda Murphy <helloamandamurphy@gmail.com>
2019-06-27 23:25:54 -0600

    Create Sessions Controller, Sessions view--signin, and routes. User creation, signin, and logout currently functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
+
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy', via: 'delete'
 end
