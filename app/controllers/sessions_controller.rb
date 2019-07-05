5eb284f68f777921f4d229fc1d7e5388f51fb7f1
Amanda Murphy <helloamandamurphy@gmail.com>
2019-07-05 14:13:55 -0600

    Add notes to controllers, change submit button text on new user form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
+  #loads login page
   def new
     @user = User.new
   end
 
+  #creates session for existing user
   def create
+    #finds user by email
     @user = User.find_by(email: params[:user][:email])
+    #if the user exists, and the password matches,
+    #creates a new session, if not, it redirects to login page
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
@@ -13,6 +18,7 @@ class SessionsController < ApplicationController
     end
   end
 
+  #logs out user
   def destroy
     session.delete :user_id
     redirect_to '/'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  #loads sign up form
   def new
     @user = User.new
   end
 
+  #sign up--saves and logs in user if required attributes are provided
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,6 +15,7 @@ class UsersController < ApplicationController
     end
   end
 
+  #loads user show page
   def show
     if User.find_by(id: params[:id]) && session[:user_id]
       @user = User.find_by(id: params[:id])
